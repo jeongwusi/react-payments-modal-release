7889f66e2a44aefe3542a59d9ad14f90ca766638
jeongwusi <jeongwusi@gmail.com>
2023-05-08 23:02:37 +0900

    📦 chore: 버전 1.1.1이라는 이름으로 배포

--- dist/Modal/Modal.d.ts
+++ dist/Modal/Modal.d.ts
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 interface CardCompanyModalProps extends PropsWithChildren {
+    onOpen?: () => void;
     onClose: () => void;
 }
-declare const Modal: ({ onClose, children }: CardCompanyModalProps) => JSX.Element;
+declare const Modal: ({ onOpen, onClose, children }: CardCompanyModalProps) => JSX.Element;
 export default Modal;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
-export { default as Modal } from "./Modal/Modal";
+import Modal from "./Modal/Modal";
+export default Modal;
